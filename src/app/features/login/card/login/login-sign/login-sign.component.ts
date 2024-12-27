11c428c8a860a8ad904b9dfb7407d5a1255a34e7
willians silva <willians.silva@essentia.com.br>
2024-12-27 16:59:04 -0300

    Changes to be committed: 	modified:   src/app/features/login/card/login/login-sign/login-sign.component.html 	modified:   src/app/features/login/card/login/login-sign/login-sign.component.ts 	deleted:    src/app/shared/components/main-button/main-button.component.html 	deleted:    src/app/shared/components/main-button/main-button.component.scss 	deleted:    src/app/shared/components/main-button/main-button.component.spec.ts 	deleted:    src/app/shared/components/main-button/main-button.component.ts

--- src/app/features/login/card/login/login-sign/login-sign.component.ts
+++ src/app/features/login/card/login/login-sign/login-sign.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { MainButtonComponent } from '@components/main-button/main-button.component';
 
 @Component({
   selector: 'component-login-sign',
-  imports: [MainButtonComponent],
+  imports: [],
   templateUrl: './login-sign.component.html',
   styleUrl: './login-sign.component.scss'
 })
